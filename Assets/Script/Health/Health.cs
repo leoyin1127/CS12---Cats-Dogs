f0aa6e303a7459a546666e5bc1478a34a104be94
Leo Yin <71328966+leoyin1127@users.noreply.github.com>
2023-02-22 14:29:02 -0500

    bug fixing

--- Assets/Script/Health/Health.cs
+++ Assets/Script/Health/Health.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    // private Animator anim;
+    // private bool dead;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        currentHealth = startingHealth;
+        // anim = GetComponent<Animator>();
     }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            // anim.SetTrigger("hurt");
+            //iframes
+        }
+        else
+        {
+            // if (!dead)
+            // {
+            //     anim.SetTrigger("die");
+            //     GetComponent<PlayerMovement>().enabled = false;
+            //     dead = true;
+            // }
+        }
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+            TakeDamage(1);
+    }
+    // public void AddHealth(float _value)
+    // {
+    //     currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    // }
 }
