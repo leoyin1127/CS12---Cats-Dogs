b882701777f730c838a4187f1313fa703be67a97
cwsengai <124812208+cwsengai@users.noreply.github.com>
2023-02-22 13:27:49 -0500

    change

--- Assets/Script/catanddog.cs
+++ Assets/Script/catanddog.cs
@@ -8,16 +8,41 @@ public class catanddog : MonoBehaviour
     int point = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Renderer ren;
+
+        void invisible()
+        {
+            ren = GetComponent<Renderer>();
+            ren.enabled = false;
+        }
+
+        void visible()
+        {
+            ren = GetComponent<Renderer>();
+            ren.enabled = true;
+        }
+
         if (collision.gameObject.tag == "Enemy")
         {
-            Destroy(gameObject);
-            point += 1;            
+            //Destroy(gameObject);
+            //gameObject.SetActive(false);
+            invisible();
+            point += 1;
+            //gameObject.SetActive(true);
+            visible();
+
         }
         if (collision.gameObject.tag == "Ground")
         {
-            Destroy(gameObject);
-            point -= 1;            
+            //Destroy(gameObject);
+            //gameObject.SetActive(false);
+            invisible();
+            point -= 1;
+            visible();
+            //gameObject.SetActive(true);
         }
         Debug.Log(point);
+
+       
     }
 }
