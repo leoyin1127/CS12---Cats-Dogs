a3bedb4cda4d0df324f99a72de627789e3a39200
Leo Yin <71328966+leoyin1127@users.noreply.github.com>
2023-02-22 20:11:27 -0500

    fallen lightning

--- Assets/Script/LightningController.cs
+++ Assets/Script/LightningController.cs
@@ -3,25 +3,59 @@ using UnityEngine;
 public class LightningController : MonoBehaviour
 {
     [SerializeField] private float damage;
+    [SerializeField] private float minX; // the minimum x position for the lightning to spawn
+    [SerializeField] private float maxX; // the maximum x position for the lightning to spawn
+    [SerializeField] private float Y; // the Y position for the lightning to spawn
+    
+    [SerializeField] private float fallSpeed; // the speed at which the lightning falls
+    private bool isFalling = false; // whether or not the lightning is currently falling
 
-   
-   
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Update()
     {
-        if (collision.tag == "Enemy")
+
+        if (!isFalling)
         {
-            collision.GetComponent<Health>().TakeDamage(damage);
-            Destroy(gameObject);
+            isFalling = true;
+            float x = Random.Range(minX, maxX);
+            transform.position = new Vector2(x, transform.position.y);
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.position -= new Vector3(0, fallSpeed * Time.deltaTime, 0);
         }
 
+        if (transform.position.y < -10f)
+        {
+            // gameObject.SetActive(false);
+            isFalling = false;
+            float x = Random.Range(minX, maxX);
+            transform.position = new Vector2(x, Y);
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<Health>().TakeDamage(damage);
+            transform.position = new Vector2(0, 20f);
+            isFalling = false;
+            float x = Random.Range(minX, maxX);
+            transform.position = new Vector2(x, Y);
+
+        }
+    }
+
+   
 }
 
- // private void OnCollisionEnter2D(Collision2D collision)
-    // {
-    //     if (collision.gameObject.CompareTag("Enemy"))
-    //     {
-    //         collision.gameObject.GetComponent<Health>().TakeDamage(damage);
-    //         Destroy(gameObject);
-    //     }
-    // }
+//  private void OnDisable()
+//     {
+//         if  (gameObject.SetActive(false))
+//         {
+//              gameObject.SetActive(true);
+//               transform.position = new Vector2(0, 20f); // regenerate the lightning above the screen
+//         }
+       
+//     }
