a296ae95f6cc1df0335106a1dad8e169dd7bd936
cwsengai <124812208+cwsengai@users.noreply.github.com>
2023-02-24 13:23:38 -0500

    change

--- Assets/Script/mainmenu.cs
+++ Assets/Script/mainmenu.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class mainmenu : MonoBehaviour {
+    
+    //Get into play scene when click on play button
     public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    //Display "Quit game!" on console when click on quit button
     public void QuitGame ()
     {
-        string quit = "Quit game!";
-        Debug.Log(quit);
+        string quit = "Quit game!"; //making a string variable to display
+        Debug.Log(quit); //display variable on console
         Application.Quit();
     }
 }
